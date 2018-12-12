ac00c37fd8e70f7650555495671c215a75ed1df8
Gauthier Fiorentino <gauthier.fiorentino@epita.com>
2018-12-12 08:37:47 +0100

    Update: Wednesday 12 2018 at 08:37:47 CET (07:37:47 UTC).

--- confExport.sh
+++ confExport.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-CONFS="/home/gauthier.fiorentino/afs/.confs/"
-GITREPO="/home/gauthier.fiorentino/afs/gauthier.fiorentino/Config/"
+CONFS="/home/mintoo200/"
+GITREPO="/home/mintoo200/Documents/Dotfiles/"
 
-FILELIST="$CONFS/bashrc $CONFS/vimrc $CONFS/confExport.sh $CONFS/Xdefaults $CONFS/vim $CONFS/i3lock.png $CONFS/background.png $CONFS/gitconfig_confExport"
+FILELIST="$CONFS/bashrc $CONFS/vimrc $CONFS/vim $CONFS/i3lock.png $CONFS/background.png $CONFS/gitconfig_confExport"
 
 if [ $# -eq 0 ]; then
     cp -u -r -t "$GITREPO" $FILELIST 1> /dev/null
@@ -11,12 +11,8 @@ else
     cp -u -r "$CONFS$1" "$GITREPO" 1> /dev/null
 fi
 
-rm -rf /home/gauthier.fiorentino/.gitconfig
-ln -s ${CONFS}/gitconfig_confExport /home/gauthier.fiorentino/.gitconfig
 cd "$GITREPO"
 git add -A 1> /dev/null
 git commit -m  "Update: $(date +"%A %d %Y at %T %Z") ($(date -u +"%T %Z"))." 1> /dev/null
 git push 1> /dev/null
-rm -rf /home/gauthier.fiorentino/.gitconfig
-ln -s ${CONFS}/gitconfig /home/gauthier.fiorentino/.gitconfig
 exit 0
